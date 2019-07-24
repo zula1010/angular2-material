856d705e14a35727c8cec1a9a2408da88c18ed78
zula1010 <munkhzulk@gmail.com>
2019-07-23 17:31:06 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular2-material
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular2-material)
---
+++ app/job.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'job',
+  templateUrl: 'job.html',
+  styleUrls: ['job.css'],
+})
+export class Job {}
